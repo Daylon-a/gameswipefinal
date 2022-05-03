23405e18e1097e93bae72a1be9238de7e7eea898
Daylon-a <adkisondkad@gmail.com>
2022-05-03 01:07:48 -0500

    Working Tabs, no bootstrap/vue functionality

--- gamecarousel/views.py
+++ gamecarousel/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
 from .models import GameData
 from django.contrib.auth.models import User
 
@@ -12,41 +13,30 @@ def home(request):
 def mainpage(request):
 
     print('enter if statement')
+    
     if request.method == 'GET':
         print('the get request was followed successfully!')
         username = request.user.get_username()
-
-        selectedgame = GameData.objects.order_by('?')[0:1].get()
-        gname = getattr(selectedgame, 'gname', 'Not Found')
-        gdescription = getattr(selectedgame, 'description', 'No Description Found')
+        selectedgame_list = GameData.objects.order_by('?')[0:4]
+        for game in selectedgame_list.iterator():
+            print (game)
+            print (game.gname)
+            print (game.description)
     
-        secondselectedgame = GameData.objects.order_by('?')[1:2].get()
-        secondgname = getattr(secondselectedgame, 'gname', 'Not Found')
-        secondgdescription = getattr(secondselectedgame, 'description', 'No Description Found')
-
-        thirdselectedgame = GameData.objects.order_by('?')[2:3].get()
-        thirdgname = getattr(thirdselectedgame, 'gname', 'Not Found')
-        thirdgdescription = getattr(thirdselectedgame, 'description', 'No Description Found')
-
-        fourthselectedgame = GameData.objects.order_by('?')[3:4].get()
-        fourthgname = getattr(fourthselectedgame, 'gname', 'Not Found')
-        fourthgdescription = getattr(fourthselectedgame, 'description', 'No Description Found')
-    
-
         return render(request, 'mainpage.html', 
-        {'selectedgame': selectedgame, 
-        'gname': gname, 
-        'gdescription': gdescription, 
-        'username': username,
-        'secondselectedgame': secondselectedgame,
-        'secondgname': secondgname,
-        'secondgdescription': secondgdescription,
-        'thirdselectedgame': thirdselectedgame,
-        'thirdgname': thirdgname,
-        'thirdgdescription': thirdgdescription,
-        'fourthselectedgame': fourthselectedgame,
-        'fourthgname': fourthgname,
-        'fourthgdescription': fourthgdescription})
+        { 
+        'gamename': game.gname, 'gamedescription': game.description, 'gamelist': selectedgame_list
+        }) 
     
-    else:
-        print('the post request was not followed successfully')
+
+class AjaxHandlerView(View):
+    def get(self, request):
+        text = request.GET.get('button_text')
+        print()
+        print(text)
+        print()
+        return render(request, 'mainpage.html')
+            
+
+
+    
