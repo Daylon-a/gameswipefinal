cfc4616ae172d94a686b9f2e98a4db57fdf8ed70
Daylon-a <adkisondkad@gmail.com>
2022-05-03 07:44:57 -0500

    This version has the single color theme Accordian with game filtering

--- gamecarousel/views.py
+++ gamecarousel/views.py
@@ -13,20 +13,94 @@ def home(request):
 def mainpage(request):
 
     print('enter if statement')
+    system = request.POST.get('genrebutton', None)
     
-    if request.method == 'GET':
-        print('the get request was followed successfully!')
+    
+    
+    if request.method == 'POST' and system == "RPG":
+        print('the RPG only request has been satisfied ;)')
         username = request.user.get_username()
-        selectedgame_list = GameData.objects.order_by('?')[0:4]
-        for game in selectedgame_list.iterator():
-            print (game)
-            print (game.gname)
-            print (game.description)
-    
+        selectedgame = GameData.objects.filter(genre='RPG').order_by('?')[0:1].get()
+        secondselectedgame = GameData.objects.filter(genre='RPG').order_by('?')[1:2].get()
+        thirdselectedgame = GameData.objects.filter(genre='RPG').order_by('?')[2:3].get()
+        fourthselectedgame = GameData.objects.filter(genre='RPG').order_by('?')[3:4].get()
+
         return render(request, 'mainpage.html', 
-        { 
-        'gamename': game.gname, 'gamedescription': game.description, 'gamelist': selectedgame_list
-        }) 
+        {'selectedgame': selectedgame,
+         'username': username, 
+        'secondselectedgame': secondselectedgame,
+        'thirdselectedgame': thirdselectedgame,
+        'fourthselectedgame': fourthselectedgame,
+        'system': system})
+
+    if request.method == 'POST' and system == "Action-Adventure":
+        print('the Action-Adventure only request has been satisfied ;)')
+        username = request.user.get_username()
+        selectedgame = GameData.objects.filter(genre='Action-Adventure').order_by('?')[0:1].get()
+        secondselectedgame = GameData.objects.filter(genre='Action-Adventure').order_by('?')[1:2].get()
+        thirdselectedgame = GameData.objects.filter(genre='Action-Adventure').order_by('?')[2:3].get()
+        fourthselectedgame = GameData.objects.filter(genre='Action-Adventure').order_by('?')[3:4].get()
+
+        return render(request, 'mainpage.html', 
+        {'selectedgame': selectedgame,
+         'username': username, 
+        'secondselectedgame': secondselectedgame,
+        'thirdselectedgame': thirdselectedgame,
+        'fourthselectedgame': fourthselectedgame,
+        'system': system})
+
+    if request.method == 'POST' and system == "Shooter":
+        print('the Sports only request has been satisfied ;)')
+        username = request.user.get_username()
+        selectedgame = GameData.objects.filter(genre='Shooter').order_by('?')[0:1].get()
+        secondselectedgame = GameData.objects.filter(genre='Shooter').order_by('?')[1:2].get()
+        thirdselectedgame = GameData.objects.filter(genre='Shooter').order_by('?')[2:3].get()
+        fourthselectedgame = GameData.objects.filter(genre='Shooter').order_by('?')[3:4].get()
+
+        return render(request, 'mainpage.html', 
+        {'selectedgame': selectedgame,
+         'username': username, 
+        'secondselectedgame': secondselectedgame,
+        'thirdselectedgame': thirdselectedgame,
+        'fourthselectedgame': fourthselectedgame,
+        'system': system})
+
+    if request.method == 'POST' and system == "Random":
+        print('the random get request was followed successfully!')
+        username = request.user.get_username()
+        selectedgame = GameData.objects.order_by('?')[0:1].get()
+        secondselectedgame = GameData.objects.order_by('?')[1:2].get()
+        thirdselectedgame = GameData.objects.order_by('?')[2:3].get()
+        fourthselectedgame = GameData.objects.order_by('?')[3:4].get()
+
+        return render(request, 'mainpage.html', 
+        {'selectedgame': selectedgame,
+         'username': username, 
+        'secondselectedgame': secondselectedgame,
+        'thirdselectedgame': thirdselectedgame,
+        'fourthselectedgame': fourthselectedgame,
+        'system': system})
+
+    elif request.method == 'GET':
+        context = {}
+        print('the normal get request was followed successfully!')
+        
+        context[system] = system
+        username = request.user.get_username()
+        selectedgame = GameData.objects.order_by('?')[0:1].get()
+        secondselectedgame = GameData.objects.order_by('?')[1:2].get()
+        thirdselectedgame = GameData.objects.order_by('?')[2:3].get()
+        fourthselectedgame = GameData.objects.order_by('?')[3:4].get()
+
+        return render(request, 'mainpage.html', 
+        {'selectedgame': selectedgame,
+         'username': username, 
+        'secondselectedgame': secondselectedgame,
+        'thirdselectedgame': thirdselectedgame,
+        'fourthselectedgame': fourthselectedgame,
+        'system': system})
+    else:
+        print('the post request was not followed successfully')
     
 
 class AjaxHandlerView(View):
